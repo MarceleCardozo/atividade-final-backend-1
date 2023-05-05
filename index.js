4e6d5ea7b816179d273a18b0bd264e0458447f17
MarceleDutra Cardozo <marcelecardozo@gmail.com>
2023-05-05 19:57:59 -0300

    Middleware para validar dados inseridos pelo usuário para criação de conta

--- index.js
+++ index.js
@@ -4,18 +4,35 @@ const app = express();
 
 app.use(express.json());
 
+//Middleware para validar dados inseridos pelo usuário para criação de conta
+function validateEnteredDataForAccountCreation(request, response, next) {
+  const name = request.body.name;
+  const email = request.body.email;
+  const password = request.body.password;
+
+  if (!name || !email || !password) {
+    return response.status(401).json("Por favor, preencha todos os campos.");
+  } else {
+    next();
+  }
+}
+
 let users = [];
 
 //Criação de conta
-app.post("/users", (request, response) => {
-  const user = request.body;
-  users.push({
-    name: user.name,
-    email: user.email,
-    password: user.password,
-  });
-  return response.status(201).json("Conta criada com sucesso!");
-});
+app.post(
+  "/users",
+  validateEnteredDataForAccountCreation,
+  (request, response) => {
+    const user = request.body;
+    users.push({
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    });
+    return response.status(201).json("Conta criada com sucesso!");
+  }
+);
 
 app.get("/", (request, response) => {
   return response.json("OK");
